5fd2f9c4a069be4a9cd8d69d558bee7b1d0c1605
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2023-03-22 16:52:41 -0700

    Update MediaDisplayManager.cs

--- Runtime/Scripts/MediaDisplayManager.cs
+++ Runtime/Scripts/MediaDisplayManager.cs
@@ -1,115 +1,120 @@
 using UnityEngine;
 
-// This namespace contains the utility classes for creating a demo screen in Unity.
+// Contains utility classes for creating and managing media displays in Unity.
 namespace MediaDisplay
 {
-    // This class provides utility methods for setting up a demo screen to display test images, videos, or webcam streams.
+    // Provides utility methods for setting up and managing a media display screen in Unity.
     public static class MediaDisplayManager
     {
         /// <summary>
-        /// Sets the texture for the demo screen object.
+        /// Sets the texture for the screen object and raises a texture change event.
         /// </summary>
-        /// <param name="demoScreenObject">The GameObject to be used as the demo screen.</param>
-        /// <param name="displayTexture">The Texture to display on the demo screen.</param>
-        public static void SetDemoScreenTexture(GameObject demoScreenObject, Texture displayTexture)
+        /// <param name="screenObject">The GameObject to be used as the screen.</param>
+        /// <param name="displayTexture">The Texture to display on the screen.</param>
+        public static void SetScreenTexture(GameObject screenObject, Texture displayTexture)
         {
-            // Get the MeshRenderer component from the screen object.
-            MeshRenderer meshRenderer = demoScreenObject.GetComponent<MeshRenderer>();
-            if (meshRenderer == null) return;
+            // Attempt to get the MeshRenderer component from the screen object
+            if (screenObject.TryGetComponent<MeshRenderer>(out MeshRenderer meshRenderer))
+            {
+                // Create a new material with the Unlit/Texture shader and set its main texture
+                Material screenMaterial = new Material(Shader.Find("Unlit/Texture"))
+                {
+                    mainTexture = displayTexture
+                };
 
-            // Set the texture and shader for the screen object's material.
-            Material screenMaterial = new Material(Shader.Find("Unlit/Texture"));
-            screenMaterial.mainTexture = displayTexture;
-            meshRenderer.material = screenMaterial;
-            // Raise the event to inform other scripts that the texture has been changed
-            TextureChangeEvent.RaiseMainTextureChangedEvent(meshRenderer.material);
+                // Assign the material to the mesh renderer
+                meshRenderer.material = screenMaterial;
+
+                // Raise a texture change event to inform other scripts
+                TextureChangeEvent.RaiseMainTextureChangedEvent(meshRenderer.material);
+            }
         }
 
         /// <summary>
-        /// Updates the rotation, scale, and position of the demo screen object based on the new texture dimensions.
+        /// Updates the screen object's rotation, scale, and position based on the display texture and mirror settings.
         /// </summary>
-        /// <param name="demoScreenObject">The GameObject to be used as the demo screen.</param>
-        /// <param name="displayTexture">The Texture displayed on the demo screen.</param>
+        /// <param name="screenObject">The GameObject to be used as the screen.</param>
+        /// <param name="displayTexture">The Texture displayed on the screen.</param>
         /// <param name="mirrorScreen">Optional parameter to mirror the screen horizontally. Default is false.</param>
-        public static void UpdateDemoScreenTransform(GameObject demoScreenObject, Texture displayTexture, bool mirrorScreen = false)
+        public static void UpdateScreenTransform(GameObject screenObject, Texture displayTexture, bool mirrorScreen = false)
         {
-            // Set the rotation of the screen object based on the mirrorScreen parameter.
-            demoScreenObject.transform.rotation = Quaternion.Euler(0, mirrorScreen ? 180f : 0f, 0);
+            // Get the width and height of the display texture
+            float width = displayTexture.width;
+            float height = displayTexture.height;
 
-            // Set the scale of the screen object based on the displayTexture dimensions.
-            demoScreenObject.transform.localScale = new Vector3(displayTexture.width, displayTexture.height, mirrorScreen ? -1f : 1f);
-
-            // Set the position of the screen object.
-            demoScreenObject.transform.position = new Vector3(displayTexture.width / 2, displayTexture.height / 2, 1);
+            // Set the rotation, scale, and position of the screen object
+            screenObject.transform.rotation = Quaternion.Euler(0, mirrorScreen ? 180f : 0f, 0);
+            screenObject.transform.localScale = new Vector3(width, height, mirrorScreen ? -1f : 1f);
+            screenObject.transform.position = new Vector3(width / 2, height / 2, 1);
         }
 
         /// <summary>
-        /// Initializes the camera used for displaying the demo screen.
+        /// Initializes the camera used for displaying the screen.
         /// </summary>
-        /// <param name="demoCameraObject">The GameObject with a Camera component to be used for displaying the demo screen.</param>
-        /// <param name="screenDimensions">The dimensions of the demo screen.</param>
-        public static void InitializeCamera(GameObject demoCameraObject, Vector2Int screenDimensions)
+        /// <param name="cameraObject">The GameObject with a Camera component to be used for displaying the screen.</param>
+        /// <param name="screenDimensions">The dimensions of the screen.</param>
+        public static void InitializeCamera(GameObject cameraObject, Vector2Int screenDimensions)
         {
-            // Get the Camera component from the camera object.
-            Camera camera = demoCameraObject.GetComponent<Camera>();
-            if (camera == null) return;
+            // Attempt to get the Camera component from the camera object
+            if (cameraObject.TryGetComponent<Camera>(out Camera camera))
+            {
+                // Set the position of the camera object
+                Vector3 position = new Vector3(screenDimensions.x / 2, screenDimensions.y / 2, -10f);
+                cameraObject.transform.position = position;
 
-            // Set the position of the camera object.
-            demoCameraObject.transform.position = new Vector3(screenDimensions.x / 2, screenDimensions.y / 2, -10f);
-
-            // Set the camera to orthographic mode and set its size.
-            camera.orthographic = true;
-            camera.orthographicSize = screenDimensions.y / 2;
+                // Configure the camera for orthographic mode and set its size
+                camera.orthographic = true;
+                camera.orthographicSize = screenDimensions.y / 2;
+            }
         }
 
         /// <summary>
-        /// Initializes a webcam stream with the specified settings.
+        /// Initializes and plays a webcam stream with the specified settings.
         /// </summary>
-        /// <param name="webcamTexture">The WebCamTexture instance to be initialized and played.</param>
+        /// <param name="webcamTexture">A reference to the WebCamTexture instance to be initialized and played.</param>
         /// <param name="deviceName">The name of the webcam device to be used for streaming.</param>
         /// <param name="webcamDimensions">The desired resolution of the webcam stream.</param>
-        /// <param name="webcamFrameRate">The desired frame rate of the webcam stream.</param>
+        /// <param name="webcamFrameRate">The desired frame rate of the webcam stream. Default is 60.</param>
         /// <returns>Returns true if the webcam stream has started playing, false otherwise.</returns>
         public static bool InitializeWebcam(ref WebCamTexture webcamTexture, string deviceName, Vector2Int webcamDimensions, int webcamFrameRate = 60)
         {
-            // Stop the webcam stream if it's already playing.
+            // If the webcam texture is not null and it is playing, stop it
             if (webcamTexture != null && webcamTexture.isPlaying)
             {
                 webcamTexture.Stop();
             }
 
-            // Create a new WebCamTexture instance with the specified settings.
+            // Create a new WebCamTexture instance with the specified settings
             webcamTexture = new WebCamTexture(deviceName, webcamDimensions.x, webcamDimensions.y, webcamFrameRate);
 
-            // Start playing the webcam stream.
+            // Start playing the webcam stream
             webcamTexture.Play();
 
-            // Return true if the webcam stream has started playing, false otherwise.
+            // Return true if the webcam stream has started playing, false otherwise
             return webcamTexture.isPlaying;
         }
 
         /// <summary>
-        /// Updates the texture, transform, and camera of the demo screen object.
+        /// Updates the texture, transform, and camera of the screen object.
         /// </summary>
-        /// <param name="demoScreenObject">The GameObject to be used as the demo screen.</param>
-        /// <param name="displayTexture">The Texture displayed on the demo screen.</param>
-        /// <param name="cameraObject">The GameObject with a Camera component to be used for displaying the demo screen.</param>
+        /// <param name="screenObject">The GameObject to be used as the screen.</param>
+        /// <param name="displayTexture">The Texture displayed on the screen.</param>
+        /// <param name="cameraObject">The GameObject with a Camera component to be used for displaying the screen.</param>
         /// <param name="mirrorScreen">Optional parameter to mirror the screen horizontally. Default is false.</param>
-        public static void UpdateScreenTexture(GameObject demoScreenObject, Texture displayTexture, GameObject cameraObject, bool mirrorScreen = false)
+        public static void UpdateScreenTexture(GameObject screenObject, Texture displayTexture, GameObject cameraObject, bool mirrorScreen = false)
         {
-            // Update the texture of the demo screen object.
-            SetDemoScreenTexture(demoScreenObject, displayTexture);
+            // Update the texture of the screen object
+            SetScreenTexture(screenObject, displayTexture);
 
-            // Update the transform of the demo screen object based on the new texture dimensions.
-            UpdateDemoScreenTransform(demoScreenObject, displayTexture, mirrorScreen);
+            // Update the transform of the screen object based on the new texture dimensions and mirror settings
+            UpdateScreenTransform(screenObject, displayTexture, mirrorScreen);
 
-            // Get the screen dimensions from the updated demo screen object.
-            int screenWidth = (int)demoScreenObject.transform.localScale.x;
-            int screenHeight = (int)demoScreenObject.transform.localScale.y;
-            Vector2Int screenDimensions = new Vector2Int(screenWidth, screenHeight);
+            // Get the screen dimensions from the updated screen object
+            Vector2Int screenDimensions = new Vector2Int(displayTexture.width, displayTexture.height);
 
-            // Initialize the camera for displaying the demo screen.
+            // Initialize the camera for displaying the screen
             InitializeCamera(cameraObject, screenDimensions);
         }
+
     }
 }
