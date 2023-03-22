f7934839b1b0d62021c7a1f0b0f6d471185d4961
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2023-03-22 11:05:38 -0700

    Create TextureChangeEvent.cs

---
+++ Runtime/Scripts/TextureChangeEvent.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace MediaDisplay
+{
+    public class TextureChangeEvent : MonoBehaviour
+    {
+        // Define a delegate with the desired signature
+        public delegate void OnMainTextureChangedDelegate(Material material);
+
+        // Create a static event with the delegate type
+        public static event OnMainTextureChangedDelegate OnMainTextureChanged;
+
+        // Method to call when the mainTexture has been changed
+        public static void RaiseMainTextureChangedEvent(Material material)
+        {
+            OnMainTextureChanged?.Invoke(material);
+        }
+    }
+}
