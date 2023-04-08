92a9a1e708ebe15b66508a3da547c3c95959a2f8
Christian J. Mills <9126128+cj-mills@users.noreply.github.com>
2023-04-08 15:18:05 -0700

    Create ScreenResolutionWatcher.cs

---
+++ Runtime/Scripts/ScreenResolutionWatcher.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+// Contains utility classes for creating and managing media displays in Unity.
+namespace CJM.MediaDisplay
+{
+    public class ScreenResolutionWatcher : MonoBehaviour
+    {
+        // Define a delegate and an event for the resolution change
+        public delegate void ResolutionChangedDelegate(Resolution newResolution);
+        public event ResolutionChangedDelegate OnResolutionChanged;
+
+        private Resolution _currentResolution;
+
+        private void Start()
+        {
+            // Initialize the current resolution value
+            _currentResolution = Screen.currentResolution;
+        }
+
+        private void Update()
+        {
+            // Check if the resolution has changed
+            if (_currentResolution.width != Screen.currentResolution.width || _currentResolution.height != Screen.currentResolution.height)
+            {
+                // Update the current resolution value
+                _currentResolution = Screen.currentResolution;
+
+                // Invoke the event
+                OnResolutionChanged?.Invoke(_currentResolution);
+            }
+        }
+    }
+}
